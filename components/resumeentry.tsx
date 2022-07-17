a221ef707f5308ca672c4d1a4b0c22fc0b445426
Shreyas Kishore <i@0xsk.io>
2022-07-17 14:22:22 -0400

    Added ResumeEntry type

---
+++ components/resumeentry.tsx
@@ -0,0 +1,8 @@
+type ResumeEntry = {
+  title?: string | JSX.Element;
+  subtitle?: string | JSX.Element;
+  meta?: string | JSX.Element;
+  description?: string | string[] | JSX.Element | JSX.Element[];
+};
+
+export type { ResumeEntry };
